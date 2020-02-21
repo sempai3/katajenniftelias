9871a3bde10323d386b3967bd24e98471f4f9161
jenn-lang <jennifervallejos08@gmail.com>
2020-02-21 14:24:46 -0300

    hice una branch y comite lo que hay que hacer para resolverlo

---
+++ index.js
@@ -0,0 +1,11 @@
+//dados dos strings hacer una funcion que devuelva si son anagramas;ESTE EJERCIO BUSCAR PRACTICAR.
+
+//condiciones que use las mimas letras que no repita la palabra
+
+//lafuncion sera una booleana
+
+//COMO SOLUCIONARON EN CLASE:EJ 1 LAS VARIABLES,IF,2 FOR ,IF.EJ 2 PREGUNTAR A JOWIE EJ3 
+
+
+//function sonAnagramas("roma","ramo"){}
+
